b9f932893a36991f819498ffec8814dbe17cffbb
Carson Reader <carsonthemonkey@gmail.com>
2023-10-27 14:38:05 -0700

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# This is a simple python script for testing that your environment is configured properly
+
+# Make sure you are using a virtual environment so dependencies stay isolated
+import scipy
+
+print("Hello World!")
+print(f"SciPy version: {scipy.__version__}")
