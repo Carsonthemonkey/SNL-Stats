82d3c190f99955199127369f9ecffbb18689abc2
Carson Reader <carsonthemonkey@gmail.com>
2023-10-28 12:32:32 -0700

    Adds HTTPS based api test for the youtube api

---
+++ youtube_api_test.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+
+# This is using HTTPS request rather than the python wrapper for the API for now. The python library seems overcomplicated to me, but we may need it down the line
+
+load_dotenv() # load .env file
+
+API_KEY = os.getenv("YOUTUBE_API_KEY") # Get api key
+assert API_KEY is not None
+VIDEO_ID = "dQw4w9WgXcQ" 
+
+# encode the URL with the data we want
+url = f"https://www.googleapis.com/youtube/v3/videos?id={VIDEO_ID}&part=statistics&key={API_KEY}"
+
+# call the api with a timeout of 15 seconds
+response = requests.get(url, timeout=15)
+
+# convert the json response to a python dictionary
+data = response.json()
+assert isinstance(data, dict)
+
+print(json.dumps(data, indent=4))
