27eca2609d98ee7ed18c1c684f9288873ed219ba
Kat Berge <kat.n.berge@gmail.com>
2023-11-07 16:05:09 -0800

    removes unneeded example from schema

--- schema.py
+++ schema.py
@@ -7,20 +7,3 @@ class Video(BaseModel):
     view_count: int
     like_count: int
     comment_count: int
-
-
-# Example validation
-def main():
-    v = {
-        "video_id": "whee",
-        "title": "hello",
-        "duration": 6,
-        "view_count": 6,
-        "like_count": 6,
-        "comment_count": 6
-    }
-    v = Video(**v)
-    print(type(v))
-    
-if __name__ == "__main__":
-    main()
