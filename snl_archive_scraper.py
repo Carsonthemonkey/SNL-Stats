0f45c23119c683586426cf512d93fa2fbc2aaa21
Carson Reader <carsonthemonkey@gmail.com>
2023-11-08 21:14:22 -0800

    Adds functionality to archive scraper

--- snl_archive_scraper.py
+++ snl_archive_scraper.py
@@ -1,27 +1,39 @@
 import asyncio
-import requests
-from bs4 import BeautifulSoup
 import re
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from schema import Scene
+import json
 
-async def get_actors_from_episode_url(url: str) -> list:
+async def get_scenes_from_episode_url(url: str) -> list:
     res = requests.get(url, timeout=20)
-    # content is initially bytes, so must be cast as a string
-    html = res.content
-    soup = BeautifulSoup(html, 'html.parser')
-    class_regex = re.compile('card-sketch.*')
-    cards = soup.find(id='full').find_all(class_=class_regex)
-    for card in cards:
-        print(get_card_type(card))
-    # inside div id full
-    return [card.prettify() for card in cards]
-    # card is div with classes 'card' and 'card-sketch...' has class card
+    soup = BeautifulSoup(res.content, 'html.parser')
 
-def get_card_type(card) -> str:
+    # card is div with classes 'card' and 'card-sketch...' has class card
+    class_regex = re.compile('card-sketch.*')
+    # inside div id full
+    cards = soup.find(id='full').find_all(class_=class_regex)
+    return [Scene(
+        title=get_scene_title(card),
+        scene_type=get_scene_type(card),
+        cast=[]
+    ) for card in cards]
+    
+def get_scene_type(card) -> str:
     return card.find(name='table', class_='sketch-title').tbody.tr.td.a.span.contents[0]
 
+def get_scene_title(card) -> str:
+    tag = card.find(name='table', class_='sketch-title').tbody.tr.contents[1].contents
+    
+    # Monologues have no titles, so this must be handled like so
+    if not tag:
+        return None
+    # Some titles are hyperlinks in which case the inner text of the <a> tag must be extracted
+    return str(tag[0]) if isinstance(tag[0], bs4.NavigableString) else str(tag[0].contents[0])
+
 if __name__ == "__main__":
-    result = asyncio.run(get_actors_from_episode_url("http://www.snlarchives.net/Episodes/?20200307"))
+    result = asyncio.run(get_scenes_from_episode_url("http://www.snlarchives.net/Episodes/?20200307"))
     # print(result)
-    with open('index.html', 'w') as f:
-        for card in result:
-            f.write(card)
+    with open('scenes.json', 'w') as f:
+        json.dump([dict(card) for card in result], f)
