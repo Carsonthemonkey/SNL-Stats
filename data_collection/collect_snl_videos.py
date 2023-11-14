0b7dd7b8cf06c4bbe6ca89e03d58ca986d12270c
Kat Berge <kat.n.berge@gmail.com>
2023-11-14 10:15:01 -0800

    moves to next page token but isn't getting all the videos

--- data_collection/collect_snl_videos.py
+++ data_collection/collect_snl_videos.py
@@ -12,6 +12,7 @@ def get_all_channel_video_ids(username):
     playlist_id = get_uploads_playlist(username) # SNL's will be UUqFzWxSCi39LnW1JKFR3efg
     data = get_video_data(playlist_id)
     return get_video_id_list(data)
+    # TODO: also want to return the video titles with the ids
 
 
 def get_uploads_playlist(username):
@@ -35,17 +36,25 @@ def get_video_data(playlist_id):
         "key": API_KEY,
         "playlistId": playlist_id, # should be UUqFzWxSCi39LnW1JKFR3efg for SNL
         "part": "snippet",
-        "maxResults": 50 # TODO: probably want to use nextPageToken to get all videos
+        "maxResults": 2000
     }
 
     # call the api with a timeout of 15 seconds
     response = requests.get(url, params=query_params, timeout=15)
 
     # convert the json response to a python dictionary
-    data = response.json()
-    assert isinstance(data, dict)
+    data_res = response.json()
+    data = data_res["items"]
+    assert isinstance(data, list)
 
-    print(json.dumps(data, indent=4))
+    while data_res.get("nextPageToken"):
+        query_params['pageToken'] = data_res['nextPageToken']
+        print("on to " + query_params['pageToken'])
+        response = requests.get(url, params=query_params, timeout=15)
+        data_res = response.json()
+        data.extend(data_res["items"])
+
+    # print(json.dumps(data, indent=4))
     return data
 
 def get_video_id_list(data):
