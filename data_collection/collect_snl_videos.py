abe6f9c62e360d655f4c20f37f56b90e2b44591f
Kat Berge <kat.n.berge@gmail.com>
2023-11-13 21:17:16 -0800

    found and recorded uploads playlist id

--- data_collection/collect_snl_videos.py
+++ data_collection/collect_snl_videos.py
@@ -2,10 +2,27 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
-from schema import Video
 
 def main():
-    pass
+    load_dotenv() # load .env file
+
+    API_KEY = os.getenv("YOUTUBE_API_KEY") # Get api key
+    assert API_KEY is not None
+
+    # start of URL
+    url = f"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=SaturdayNightLive&key={API_KEY}"
+    # "UCqFzWxSCi39LnW1JKFR3efg" # SNL channel id
+    # gave me "uploads": "UUqFzWxSCi39LnW1JKFR3efg
+
+    # call the api with a timeout of 15 seconds
+    response = requests.get(url, timeout=15)
+
+    # convert the json response to a python dictionary
+    data = response.json()
+    assert isinstance(data, dict)
+
+    print(json.dumps(data, indent=4))
+    return data
 
 if __name__ == "__main__":
     main()
