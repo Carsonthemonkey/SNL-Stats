0b4030112045d2680af5bc369c4913c82e516db1
Carson Reader <carsonthemonkey@gmail.com>
2023-11-14 22:54:56 -0800

    reafactors function naming

--- data_collection/fuzzy_search.py
+++ data_collection/fuzzy_search.py
@@ -1,9 +1,9 @@
 from fuzzywuzzy import process
 
 
-def get_matching_title(title: str, titles: list) -> str:
+def get_matching_string(title: str, titles: list) -> str:
     return process.extractOne(title, titles)
 
 
 if __name__ == '__main__':
-    print(get_matching_title("Angry birds - SNL", ["whiteout", "Dumb Jeopardy", "Angry Birds", "Lost in New York"]))
+    print(get_matching_string("Angry birds - SNL", ["whiteout", "Dumb Jeopardy", "Angry Birds", "Lost in New York"]))
