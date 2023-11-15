1a5b46b2ef495393988c8b8f1614b9fc9c701fae
Carson Reader <carsonthemonkey@gmail.com>
2023-11-14 16:10:16 -0800

    adds argument for youtube stats

--- collection.py
+++ collection.py
@@ -2,10 +2,14 @@ import argparse
 import asyncio
 import aiohttp
 from tqdm.asyncio import tqdm
-from data_collection.snl_archive_scraper import get_all_episode_urls, get_scenes_from_episode_url
+from data_collection.snl_archive_scraper import (
+    get_all_episode_urls,
+    get_scenes_from_episode_url,
+)
 from analysis.load_data import load_scene_data
 import datetime
 import json
+
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -14,7 +18,12 @@ async def main():
         help="fetch up to date scene data instead of relying on stored data",
         action="store_true",
     )
-    parser.add_argument('--all', '-a', help="re-collect all data", action='store_true')
+    parser.add_argument(
+        "--get-stats",
+        help="get all video statistics from the youtube api",
+        action="store_true",
+    )
+    parser.add_argument("--all", "-a", help="re-collect all data", action="store_true")
 
     args = parser.parse_args()
 
@@ -28,27 +37,44 @@ async def main():
 
             def on_complete(future):
                 pbar.update(1)
+
             semaphore = asyncio.Semaphore(15)
-            tasks = [asyncio.create_task(get_scenes_from_episode_url(url, session, semaphore)) for url in urls]
+            tasks = [
+                asyncio.create_task(
+                    get_scenes_from_episode_url(url, session, semaphore)
+                )
+                for url in urls
+            ]
 
             for task in tasks:
                 task.add_done_callback(on_complete)
 
             episodes = await asyncio.gather(*tasks)
-        scenes = [dict(scene) for episode in episodes if episode is not None for scene in episode if scene is not None]
+        scenes = [
+            dict(scene)
+            for episode in episodes
+            if episode is not None
+            for scene in episode
+            if scene is not None
+        ]
 
         with open("data/scenes.json", "w", encoding="utf-8") as f:
             data = {
                 "last_collected": datetime.datetime.now().isoformat(),
-                "scene_data": scenes
+                "scene_data": scenes,
             }
             json.dump(data, f, indent=4)
     else:
         scenes = load_scene_data()
-    
 
     # Collect or load youtube stats
-
+    if args.get_stats or args.all:
+        # collect youtube data
+        pass
+    else:
+        pass
+        # load youtube data
     # Collect or load comment sentiment
+
 
 asyncio.run(main())
