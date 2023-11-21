9d2f77c621f36d8491cfa1fc31e491a402be86ab
Kat Berge <kat.n.berge@gmail.com>
2023-11-21 11:01:42 -0800

    adds boxplot of views

--- analysis/graph.py
+++ analysis/graph.py
@@ -1,3 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from analysis.load_data import load_full_data
+
+# Load data
+data = load_full_data()
+
+# find all scene types
+scene_types = data["scene_type"].unique()
+
+# box plot of views for different scence types
+fig, ax = plt.subplots()
+ax.boxplot(
+    [data[data["scene_type"] == scene_type]["view_count"] for scene_type in scene_types]
+)
+ax.set_xticklabels(scene_types)
+ax.set_ylabel("Views")
+ax.set_title("Views by Scene Type")
+plt.show()
 
