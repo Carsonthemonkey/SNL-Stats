46b4fc7b5579798eaae3d67a1cfc6749c8874d8f
Kat Berge <kat.n.berge@gmail.com>
2023-11-21 11:04:34 -0800

    adds function organization

--- analysis/graph.py
+++ analysis/graph.py
@@ -2,19 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from analysis.load_data import load_full_data
 
-# Load data
-data = load_full_data()
 
-# find all scene types
-scene_types = data["scene_type"].unique()
+def draw_all_graphs():
+    # Load data
+    data = load_full_data()
+    draw_boxplot_for_scene_type(data)
 
-# box plot of views for different scence types
-fig, ax = plt.subplots()
-ax.boxplot(
-    [data[data["scene_type"] == scene_type]["view_count"] for scene_type in scene_types]
-)
-ax.set_xticklabels(scene_types)
-ax.set_ylabel("Views")
-ax.set_title("Views by Scene Type")
-plt.show()
+def draw_boxplot_for_scene_type(data):
+    # Load data
+    data = load_full_data()
 
+    # find all scene types
+    scene_types = data["scene_type"].unique()
+
+    # box plot of views for different scence types
+    fig, ax = plt.subplots()
+    ax.boxplot(
+        [data[data["scene_type"] == scene_type]["view_count"] for scene_type in scene_types]
+    )
+    ax.set_xticklabels(scene_types)
+    ax.set_ylabel("Views")
+    ax.set_title("Views by Scene Type")
+    plt.show()
+
+if __name__ == '__main__':
+    draw_all_graphs()
