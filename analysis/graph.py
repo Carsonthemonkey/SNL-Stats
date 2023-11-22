867f1e8abb64ac4bc3a0fa7beb04455afbacf931
Kat Berge <kat.n.berge@gmail.com>
2023-11-21 16:15:21 -0800

    generalizes box plot function to use attribute from arguments

--- analysis/graph.py
+++ analysis/graph.py
@@ -6,22 +6,22 @@ from analysis.load_data import load_full_data
 def draw_all_graphs():
     # Load data
     data = load_full_data()
-    draw_boxplot_for_scene_type(data)
+    draw_boxplot_for_scene_type(data, "view_count")
 
-def draw_boxplot_for_scene_type(data):
+def draw_boxplot_for_scene_type(data, attribute):
     # Load data
     data = load_full_data()
     # find all scene types
     scene_types = set(sketch.scene_type for sketch in data if sketch.scene_type is not None)
     # box plot of views for different scence types
     boxplot_data = [
-        [sketch.view_count for sketch in data if sketch.scene_type == scene_type and sketch.view_count is not None] for scene_type in scene_types
+        [getattr(sketch, attribute) for sketch in data if sketch.scene_type == scene_type and sketch.view_count is not None] for scene_type in scene_types
     ]
     boxplot_data = list(boxplot_data)
     fig, ax = plt.subplots(figsize=(12, 5))
     ax.boxplot(boxplot_data, vert=False, labels=scene_types)
-    ax.set_title('Boxplot of View Counts by Scene Types')
-    ax.set_xlabel('View Counts (in 10,000,000s)')
+    ax.set_title('Boxplot of ' + attribute + ' by Scene Types')
+    ax.set_xlabel(attribute + ' (in 10,000,000s)')
     ax.set_ylabel('Scene Types')
     plt.show()
 
