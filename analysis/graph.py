35d3368c22ff04adbf1ab988982b3bc7f79ac50f
Kat Berge <kat.n.berge@gmail.com>
2023-11-23 19:08:18 -0600

    adds table of mean and standard devation of view counts

--- analysis/graph.py
+++ analysis/graph.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from analysis.load_data import load_full_data
 
 
-def draw_all_graphs():
+def draw_all_graphs_and_tables():
     # Load data
     data = load_full_data()
     draw_boxplot_for_scene_type(data, "view_count")
+    table_of_mean_and_std_of_views_by_scene_type(data)
 
 def draw_boxplot_for_scene_type(data, attribute):
     # check attribute is valid
@@ -28,5 +29,23 @@ def draw_boxplot_for_scene_type(data, attribute):
     # save figure
     fig.savefig('graphs/' + attribute + '_by_scene_type_boxplot.png', bbox_inches='tight')
 
+# make table of mean and std of views for each scene type
+def table_of_mean_and_std_of_views_by_scene_type(data):
+    means = []
+    sds = []
+    scene_types = set(sketch.scene_type for sketch in data if sketch.scene_type is not None)
+    for scene_type in scene_types:
+        views = [sketch.view_count for sketch in data if sketch.scene_type == scene_type and sketch.view_count is not None]
+        means.append(np.mean(views))
+        sds.append(np.std(views))
+    # print table
+    column_width = 20
+    print(f"{'Scene Type':<{column_width}} {'Mean':<{column_width}} {'Standard Deviation':<{column_width}}")
+    print(f"{'-'*column_width} {'-'*column_width} {'-'*column_width}")
+    for st, m, sd in zip(scene_types, means, sds):
+        print(f"{st:<{column_width}} {m:<{column_width}} {sd:<{column_width}}")
+    
+    
+
 if __name__ == '__main__':
-    draw_all_graphs()
+    draw_all_graphs_and_tables()
