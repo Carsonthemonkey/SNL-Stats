4e0ee3586a3e00768b156823745dbcc539cddcf6
Kat Berge <kat.n.berge@gmail.com>
2023-12-01 18:20:37 -0800

    organization and function name changes

--- analysis/test.py
+++ analysis/test.py
@@ -22,7 +22,7 @@ def test():
     
 
 def test_attribute_values_by_duration(data, attribute="view_count"):
-    vals_by_duration = get_attribute_values_by_duration(data, attribute)
+    vals_by_duration = _get_attribute_values_by_duration(data, attribute)
     # print("Printing length of vals in vals_by_duration...")
     # for vals in vals_by_duration:
     #     print(len(vals))
@@ -33,26 +33,28 @@ def test_attribute_values_by_duration(data, attribute="view_count"):
         print(stats.f_oneway(*vals_by_duration))
         print(stats.f_oneway(*vals_by_duration).pvalue)
 
+
 def test_attribute_values_by_scene_type(data, attribute="view_count"):
-    vals_by_scene_type = get_attribute_values_by_scene_type(data, attribute)
+    vals_by_scene_type = _get_attribute_values_by_scene_type(data, attribute)
     # print("Printing length of vals in vals_by_scene_type...")
     # for vals in vals_by_scene_type:
     #     print(len(vals))
     print(stats.f_oneway(*vals_by_scene_type))
     print(stats.f_oneway(*vals_by_scene_type).pvalue)
 
+
 # get array of attribute values grouped by duration
-def get_attribute_values_by_duration(data, attribute):
-    check_attribute_is_valid(data, attribute)
+def _get_attribute_values_by_duration(data, attribute):
+    _check_attribute_is_valid(data, attribute)
     # get durations
-    durations = get_durations(data)
+    durations = _get_durations(data)
     # get attribute values for each duration
     attribute_values_by_duration = []
     for d in durations:
-        attribute_values_by_duration.append(get_duration_attribute_values(data, attribute, d))
+        attribute_values_by_duration.append(_get_duration_attribute_values(data, attribute, d))
     return attribute_values_by_duration
 
-def get_durations(data):
+def _get_durations(data):
     global stored_durations
     if stored_durations is not None:
         return stored_durations
@@ -69,7 +71,7 @@ def get_durations(data):
         durations.append(min_duration + i*(max_duration-min_duration)/4)
     return durations
 
-def get_duration_attribute_values(data, attribute, duration):
+def _get_duration_attribute_values(data, attribute, duration):
     data = sorted(data, key=lambda x: x.duration) # sort data by duration
     attribute_values = []
     for d in data:
@@ -79,36 +81,39 @@ def get_duration_attribute_values(data, attribute, duration):
             break
     return attribute_values
 
-def get_attribute_values_by_scene_type(data, attribute):
-    check_attribute_is_valid(data, attribute)
+
+def _get_attribute_values_by_scene_type(data, attribute):
+    _check_attribute_is_valid(data, attribute)
     # get scene types
-    scene_types = get_scene_types(data)
+    scene_types = _get_scene_types(data)
     # get attribute values for each scene type
     attribute_values_by_scene_type = []
     for scene_type in scene_types:
-        attribute_values_by_scene_type.append(get_scene_type_attribute_values(data, attribute, scene_type))
+        attribute_values_by_scene_type.append(_get_scene_type_attribute_values(data, attribute, scene_type))
     return attribute_values_by_scene_type
 
-def get_scene_type_attribute_values(data, attribute, scene_type):
+def _get_scene_types(data):
+    scene_types = set()
+    for d in data:
+        scene_types.add(d.scene_type)
+    return scene_types
+
+def _get_scene_type_attribute_values(data, attribute, scene_type):
     attribute_values = []
     for d in data:
         if d.scene_type == scene_type and getattr(d, attribute) is not None:
             attribute_values.append(getattr(d, attribute))
     return attribute_values
 
-def get_scene_types(data):
-    scene_types = set()
-    for d in data:
-        scene_types.add(d.scene_type)
-    return scene_types
 
-def check_attribute_is_valid(data, attribute):
+def _check_attribute_is_valid(data, attribute):
     # check attribute is valid
     if not hasattr(data[0], attribute):
         raise AttributeError("Attribute " + attribute + " does not exist in data")
     # check is attribute is numeric
     if not np.issubdtype(type(getattr(data[0], attribute)), np.number):
         raise TypeError("Attribute " + attribute + " is not numeric")
+
 
 if __name__ == "__main__":
     test()
